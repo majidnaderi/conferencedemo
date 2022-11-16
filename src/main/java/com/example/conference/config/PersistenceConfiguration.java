4588b38dbd27f9b24a84ba2229c7487e4c44d7c9
Majid Naderi <majidnaderi51@gmail.com>
2022-11-16 17:58:16 +0330

    finish heroku ok

--- src/main/java/com/example/conference/config/PersistenceConfiguration.java
+++ src/main/java/com/example/conference/config/PersistenceConfiguration.java
@@ -9,13 +9,13 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/Conference_app");
-        builder.username("user1");
-        builder.password("123");
-        System.out.println("create dataSource");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/Conference_app");
+//        builder.username("user1");
+//        builder.password("123");
+//        System.out.println("create dataSource");
+//        return builder.build();
+//    }
 }
